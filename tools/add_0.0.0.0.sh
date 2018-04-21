0b8b0ff2181379415f03753018aee1bbbd9a35cb
ZeroDot1 <zerodot1@bk.ru>
2018-04-21 15:24:34 +0200

    Clean GH-Pages

--- tools/add_0.0.0.0.sh
+++
@@ -1,2 +0,0 @@
-cd '/home/user/Schreibtisch/CHB'
-sed 's/^/0\.0\.0\.0 /' domains.txt >> hostfile
--- tools/tool_to_create_sha256Sums_fom_the_URLs.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-while read -r line; do printf %s "$line" | sha256sum | cut -f1 -d' '; done < list.txt >>list_out.txt
-# sudo rm -rf list.txt
-# sudo rm -rf list_out.txt
